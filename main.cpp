42bb7c2dfd70b7bf1dddd71ed4edd3ed6049f99a
Shaganov Vyacheslav <slavikshag@gmail.com>
2023-11-10 12:55:44 +0300

    zone blh inputted

--- main.cpp
+++ main.cpp
@@ -9,16 +9,26 @@
 
 #include "creators/SatelliteGroupCreator.hpp"
 #include "satellite_motion/wrapper/Satellite.hpp"
+#include "observation/ObservationController.hpp"
 
 int main() {
     const double JD = 2460206.383;
     const double unixTimestamp = (JD - 2440587.5) * Constants::Earth::SECONDS_IN_DAY;
     
     Vector currentTime(7);
+
+    // Creating satellite group
     SatelliteGroupCreator creator("../satellites.txt");
     auto satellites = creator.create();
     std::ofstream trajectoryStream("trajectory.txt");
     trajectoryStream << satellites.size() << '\n';
+
+    // Input zone blh
+    std::ifstream spot("../spot.txt");
+    Vector zoneBLH(3);
+    spot >> zoneBLH[0] >> zoneBLH[1] >> zoneBLH[2];
+
+    ObservationController observer(zoneBLH);
 
     double step = 30;
     int hour = 3600;
@@ -33,7 +43,7 @@ int main() {
             trajectoryStream << pos[0] << ' ' << pos[1] << ' ' << pos[2] << '\n';
             Vector ecef = myEci2ecef(pos[0], pos[1], pos[2], currentTime);
             trajectoryStream << ecef[0] << ' ' << ecef[1] << ' ' << ecef[2] << '\n';
-            // observation.record(i, ecef)
+            //observer.observed(ecef);
         }
 
     }
